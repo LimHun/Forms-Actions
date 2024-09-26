8b6ba23ee464bd5f74e719864eca0d85323cc163
임훈 <tunko@kyobobook.com>
2024-09-27 01:56:04 +0900

    challenage inpu

--- app/components/form-input.tsx
+++ app/components/form-input.tsx
@@ -1,15 +1,23 @@
 interface FormInputProps {
-  type: string
-  placeholder: string
-  required: boolean
-  errors: string[]
+  icon: React.ReactNode;
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
 }
 
-export default function FormInput({ type, placeholder, required, errors }: FormInputProps) {
-  return (
-    <div className="flex flex-col gap-2">
-      <input
-        className="h-10 w-full rounded-md border-none bg-transparent p-2 ring-2 ring-neutral-200 placeholder:text-neutral-400 focus:outline-none focus:ring-4 focus:ring-orange-500"
+export default function FormInput({
+  icon,
+  type,
+  placeholder,
+  required,
+  errors,
+}: FormInputProps) {
+  return ( 
+    <div className="flex flex-row grap-2 items-center h-10 border-none bg-transparent p-3 ring-2 ring-neutral-200 placeholder:text-neutral-400 focus:outline-none focus:ring-4 focus:ring-orange-500 rounded-full">
+      {icon} 
+      <input 
+      className="focus:outline-none pl-2"
         type={type}
         placeholder={placeholder}
         required={required}
@@ -19,6 +27,6 @@ export default function FormInput({ type, placeholder, required, errors }: FormI
           {error}
         </span>
       ))}
-    </div>
-  )
+    </div> 
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
 import Image from "next/image";
 import FormInput from "./components/form-input";
+import SocialLogin from "./components/social-login";
+import FormButton from "./components/form-btn";
+
+
+const fireIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.362 5.214A8.252 8.252 0 0112 21a8.25 8.25 0 01-5.962-13.953 8.287 8.287 0 003.962 4.553A8.983 8.983 0 0115.362 5.214z"
+    />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 18a3.75 3.75 0 10-.495-7.468 5.99 5.99 0 011.925 3.547A5.975 5.975 0 0112 18z"
+    />
+  </svg>
+);
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <svg
         data-slot="icon"
         fill="red"
@@ -25,10 +50,12 @@ export default function Home() {
           d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z"
         ></path>
       </svg>
-      <FormInput type="email" placeholder="Username" required errors={[]} />
-      <FormInput type="" placeholder="Username" required errors={[]} />
-      <FormInput type="text" placeholder="Username" required errors={[]} />
-      <div>aaa</div>
+      <div className="flex flex-col gap-6 pt-6">
+      <FormInput type="email" placeholder="Email" required errors={[]} icon={fireIcon} />  
+      <FormInput type="email" placeholder="Username" required errors={[]} icon={fireIcon} />  
+      <FormInput type="email" placeholder="Password" required errors={[]} icon={fireIcon} /> 
+      <FormButton loading={false} text={"Log in"} />
+      </div>
     </div>
   );
 }
